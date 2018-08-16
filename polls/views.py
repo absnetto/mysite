ea7d63f0740c22fd9067efe4f1adb78ff5825159
absnetto <abs.netto@gmail.com>
2018-08-16 11:11:38 -0300

    Primeiro projeto em Django.

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
-
+# coding: utf-8
 # Create your views here.
+
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
+
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s" % question_id)
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("you're voting on qiestion %s" % question_id)
+
